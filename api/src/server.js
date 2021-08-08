0f89d3b6795520f78cef707b27dc61c00a05cfe4
Marwane El Kchiri <43742934+mxrwn@users.noreply.github.com>
2021-08-09 00:21:18 +0200

    setup docker/server

--- api/src/server.js
+++ api/src/server.js
@@ -10,7 +10,7 @@ const pg = require('knex')({
   client: 'pg',
   version: '9.6',      
   searchPath: ['knex', 'public'],
-  connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://example:example@localhost:5432/test'
+  connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://example:example@localhost:5432/logs'
 });
 
 
@@ -32,30 +32,19 @@ app.get('/test', (req, res) => {
 })
 
 app.get('/', async (req, res) => {
-  const result = await pg
-    .select(['uuid', 'title', 'created_at'])
-    .from('story')
-  res.json({
-      res: result
-  })
+  
 })
 
 app.get('/story/:uuid', async (req, res) => {
-  const result = await pg
-    .select(['uuid', 'title', 'created_at'])
-    .from('story')
-    .where({uuid: req.params.uuid})
-  res.json({
-      res: result
-  })
+  
 })
 
 
 async function initialiseTables() {
-  await pg.schema.hasTable('storyblock').then(async (exists) => {
+  await pg.schema.hasTable('logs').then(async (exists) => {
     if (!exists) {
       await pg.schema
-        .createTable('storyblock', (table) => {
+        .createTable('logs', (table) => {
           table.increments();
           table.uuid('uuid');
           table.string('content');
@@ -64,12 +53,12 @@ async function initialiseTables() {
           table.timestamps(true, true);
         })
         .then(async () => {
-          console.log('created table storyblock');
+          console.log('created table logs');
         });
 
     }
   });
-  await pg.schema.hasTable('story').then(async (exists) => {
+  await pg.schema.hasTable('log').then(async (exists) => {
     if (!exists) {
       await pg.schema
         .createTable('story', (table) => {
